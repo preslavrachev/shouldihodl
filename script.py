e86f02b129a343d4c9016a1f4be8916c84c0f936
Preslav Rachev <rachev.preslav@gmail.com>
2018-05-24 06:51:04 +0200

    Adds some basic labeling of the data

--- script.py
+++ script.py
@@ -2,13 +2,27 @@ import pandas as pd
 import time
 from poloniex import Poloniex
 
+# Data frame column names
+CN_HIGH = 'high'
+CN_LOW = 'low'
+CN_LABEL = 'label'
+CN_NEXT_PCT_CHANGE = 'next_4hr_pct_change'
+CN_FUTURE_PRICE_WA = 'next_week_wa'
+CN_WEIGHTED_AVERAGE = 'weightedAverage'
+
+
 def fetch_data():
     end = int(time.time())
     start = end - 100000 * 1800
     start, end
 
     polo = Poloniex()
-    df = pd.DataFrame(polo.returnChartData("USDT_BTC", period=1800, start=start, end=end))
+    df = pd.DataFrame(
+        polo.returnChartData("USDT_BTC", period=1800, start=start, end=end))
+
+    columns_to_convert_to_float = [CN_HIGH, CN_LOW, CN_WEIGHTED_AVERAGE]
+    df[columns_to_convert_to_float] = df[columns_to_convert_to_float].astype(
+        'float32')
     return df
 
 
@@ -22,17 +36,25 @@ def extend_stats(df):
 def label_data(df):
     """
     Data passed will be split into two basic groups, depending on whether the
-    50-data-point-ma is larger or smaller than the 200-data-point-ma of a future
-    date. In the default case, this will be a week (7d) into the future.
+    price at a given future time is higher or lower than the current weighted
+    average.
     """
-    pass
+    df[CN_FUTURE_PRICE_WA] = df['weightedAverage'].shift(-1 * 2 * 24 * 7)
+    df[CN_NEXT_PCT_CHANGE] = (
+        df[CN_FUTURE_PRICE_WA] / df['weightedAverage']) - 1
+    df.loc[df[CN_NEXT_PCT_CHANGE] >= 0, CN_LABEL] = 1
+    df.loc[df[CN_NEXT_PCT_CHANGE] <= 0, CN_LABEL] = -1
+
+    return df
 
 
 def main():
     df = fetch_data()
     df = extend_stats(df)
+    df = label_data(df)
 
-    print (df)
+    print(df)
+
 
 if __name__ == '__main__':
     main()
